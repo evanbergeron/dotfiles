b1ec41cca0e7be601d313fe5029df9185292a7d1
Evan Bergeron <bergeronej@gmail.com>
2017-05-15 18:49:11 -0700

    Remap Home and End to Control / Escape

--- fix_keys_shitty_x1.sh
+++ fix_keys_shitty_x1.sh
@@ -1,6 +1,18 @@
+# Fix the horrible keyboard layout of the 2nd gen Thinkpad Carbon X1.
+#
+# This will the Home and End buttons to Control when held and Escape
+# when tapped. Left shift and right shift each will generate
+# parenthesis when tapped. This mapping destroys the Home and End
+# keycodes, but I never use them for anything anyway.
+
 xcape -e 'Shift_R=Shift_R|parenright'
 xcape -e 'Shift_L=Shift_L|parenleft'
 setxkbmap -option ctrl:nocaps
 xcape -e 'Control_L=Escape'
-xmodmap -e "keycode 110 = Escape"
-xmodmap -e "keycode 115 = Escape"
+
+spare_modifier="Hyper_L"
+xmodmap -e "keycode 110 = $spare_modifier"
+xmodmap -e "keycode 115 = $spare_modifier"
+xmodmap -e "remove mod4 = $spare_modifier" # hyper_l is mod4 by default
+xmodmap -e "add Control = $spare_modifier"
+xcape -e 'Hyper_L=Escape'
